b280a5a3d2053f142c4fd2fd95ee07a8f2f31de4
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-24 22:47:25 +0100

    rss feed reader works

--- g00339629/src/pages/feeds/feeds.ts
+++ g00339629/src/pages/feeds/feeds.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FeedsProvider } from '../../providers/feeds/feeds'
 
 @IonicPage()
 @Component({
@@ -7,12 +8,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feeds.html',
 })
 export class FeedsPage {
+    status:boolean = false
+    feed = {}
+    items:any[] = []
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController
+            , public navParams: NavParams
+            , private fp:FeedsProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedsPage');
+
+    this.fp.getFeed('http://feeds.reuters.com/reuters/technologyNews')
+            .subscribe(data=> {
+                if( data.status === 'ok'){
+                    this.status = true
+                    this.feed = data.feed
+                    this.items = data.items
+                }
+            })
   }
+
+
 
 }
--- g00339629/src/providers/feeds/feeds.ts
+++ g00339629/src/providers/feeds/feeds.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 // https://stackoverflow.com/questions/44377230/using-javascript-in-ionic-2
 // https://www.npmjs.com/package/rss-to-json
@@ -8,6 +10,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FeedsProvider {
+
+    apiURL:string = "https://api.rss2json.com/v1/api.json"
+    apiKey:string = "72nhr3wkeukkx6pv9kz8p8cgu8doh9qbhrepco0x"
+    // setings
+    rss = 'rss_url='            // url to be converted to JSON (must be escaped)
+    order_by = 'order_by='      // Possible values : pubDate, author or title.
+    order_dir = 'order_dir'     // order direction 'desc' or 'asc' (default 'desc')
+    count = 'count'             // Count of feed items to return, default is 10 .
+
+    // https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FtechnologyNews
 
     constructor(public http: HttpClient
         // , private feed:Feed
@@ -21,5 +33,10 @@ export class FeedsProvider {
     //                 console.log(err)
     //                 return rss
     //         })
+
+    // https://stackoverflow.com/a/332888/5322506
+    getFeed = (rssURL:string):Observable<any> =>
+            this.http.get(`${this.apiURL}?${this.rss}${encodeURIComponent(rssURL)}`)
+                    .map(it => it)
 
 }
