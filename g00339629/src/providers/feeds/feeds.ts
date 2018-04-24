ab12620722a397a03a21dcc0ff5d02973da472d6
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-24 16:14:07 +0100

    HomePage: lazy loaded

---
+++ g00339629/src/providers/feeds/feeds.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+// https://stackoverflow.com/questions/44377230/using-javascript-in-ionic-2
+// https://www.npmjs.com/package/rss-to-json
+import{Feed}from'rss-to-json'
+
+
+@Injectable()
+export class FeedsProvider {
+
+    constructor(public http: HttpClient, private feed:Feed){
+        console.log('Hello FeedsProvider Provider');
+    }
+
+    getFeed = (link:string):JSON =>
+            this.feed.load(link, (err, rss)=>{
+                    console.log(rss)
+                    console.log(err)
+                    return rss
+            })
+
+}
