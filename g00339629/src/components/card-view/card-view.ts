06285907a1630e3c45ec940a92d8542bdf270d13
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-27 21:40:20 +0100

    function helper addeed

--- g00339629/src/components/card-view/card-view.ts
+++ g00339629/src/components/card-view/card-view.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelperFunctionProvider } from '../../providers/helper-function/helper-function';
 
 
 
@@ -12,45 +13,12 @@ export class CardViewComponent {
 
     // text: string;
 
-    constructor() {
+    constructor(
+        private helper:HelperFunctionProvider
+    ){
         console.log('Hello CardViewComponent Component');
         // this.text = 'CardViewComponent';
     }
 
-    /**
-    *   get IMBD rating
-    */
-    getRating = (str:string):number => {
-        let start = str.indexOf('IMDB')
-        let end:number
-
-        if( start > -1 ){
-            start = str.indexOf(':', start)
-            end = str.indexOf('/', start)
-            let ratingStr = str.substring(start +1, end)
-            ratingStr = ratingStr.replace(',', '.')
-            let rating = parseFloat(ratingStr)
-
-            return rating
-        }
-        return 0
-    }
-
-    /**
-    *     cleans title
-    */
-    cleanTitle = (title:string):string => {
-          let start = title.indexOf('/')
-          const end = title.indexOf('(')
-
-          if( start > end ){ start = -1 }
-
-          title = title.substring(
-                  start < 0 ? 0 : start +1,
-                  end < 0 ? title.length : end
-          )
-
-          return title;
-    }
 
 }
--- g00339629/src/providers/helper-function/helper-function.ts
+++ g00339629/src/providers/helper-function/helper-function.ts
@@ -1,17 +1,49 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the HelperFunctionProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class HelperFunctionProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello HelperFunctionProvider Provider');
-  }
+    constructor(
+        // public http: HttpClient
+    ){
+        console.log('Hello HelperFunctionProvider Provider');
+    }
+
+    /**
+    *   cleans title
+    */
+    cleanTitle = (title:string):string => {
+        let start = title.indexOf('/')
+        const end = title.indexOf('(')
+
+        if( start > end ){ start = -1 }
+
+        title = title.substring(
+                start < 0 ? 0 : start +1,
+                end < 0 ? title.length : end )
+
+        return title;
+    }
+
+    /**
+    *   get IMBD rating
+    */
+    getRating = (str:string):number => {
+        let start = str.indexOf('IMDB')
+        let end:number
+
+        if( start > -1 ){
+            start = str.indexOf(':', start)
+            end = str.indexOf('/', start)
+            let ratingStr = str.substring(start +1, end)
+            ratingStr = ratingStr.replace(',', '.')
+            let rating = parseFloat(ratingStr)
+
+            return rating
+        }
+        return 0
+    }
+
 
 }
