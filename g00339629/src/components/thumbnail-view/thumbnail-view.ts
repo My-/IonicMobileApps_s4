98c1abfd2e74881588b431747cfc5e753ca3d689
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-28 22:18:17 +0100

    fix indentation

--- g00339629/src/components/thumbnail-view/thumbnail-view.ts
+++ g00339629/src/components/thumbnail-view/thumbnail-view.ts
@@ -3,20 +3,20 @@ import { ViewActionProvider } from '../../providers/view-action/view-action';
 import { FeedsProvider } from '../../providers/feeds/feeds';
 
 @Component({
-  selector: 'thumbnail-view'
-  , templateUrl: 'thumbnail-view.html'
-  , inputs: ['items']
+    selector: 'thumbnail-view'
+    , templateUrl: 'thumbnail-view.html'
+    , inputs: ['items']
 })
 export class ThumbnailViewComponent {
 
-  text: string;
+    text: string;
 
-  constructor(
-      private view:ViewActionProvider
-      , private feeds: FeedsProvider
-  ) {
-    console.log('Hello ThumbnailViewComponent Component');
-    this.text = 'Hello World';
-  }
+    constructor(
+        private view:ViewActionProvider
+        , private feeds: FeedsProvider
+    ) {
+        console.log('Hello ThumbnailViewComponent Component');
+        this.text = 'Hello World';
+    }
 
 }
