a1078a5c3bb5ed5558cb8b9e285b4a01ed55bb18
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-29 23:27:26 +0100

    add + implement: provider responsible for rssView

---
+++ g00339629/src/providers/view-type/view-type.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class ViewTypeProvider {
+
+    /**
+    *   variable to store view type
+    */
+    public viewType:string
+
+    protected keyValue:string = 'viewType'
+
+    /**
+    *   Providers witch has view type parameter
+    */
+    constructor(
+        private storage: Storage
+    ) {
+        console.log('Hello ViewTypeProvider Provider');
+        this.loadView()
+    }
+
+    /**
+    *   Save view type
+    */
+    saveView = () => {
+        this.storage.set(this.keyValue, this.viewType)
+                .then(it => console.log('saved viewType: '+ it))
+                .catch(err => console.error('Error then saving'+ err))
+    }
+
+    /**
+    *   Load view type
+    */
+    loadView = () => {
+        this.storage.get(this.keyValue)
+                    .then(it => this.viewType = it)             // load from storage
+                    .catch(() => {
+                        this.viewType = 'Thumbnails'      // if not in storage set as default
+                        console.log('Load default: '+ this.viewType)
+                    })  // if not in storage set as default
+                    .then(() => console.log('loaded viewType: '+ this.viewType))
+                    .catch(err=> console.error('Error then loading: '+ err))
+    }
+
+}
