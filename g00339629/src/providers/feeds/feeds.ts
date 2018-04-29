fb96d8d6021f93636f7ceb0bb07736e11ea6ffa5
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-04-29 23:31:02 +0100

    clean up

--- g00339629/src/providers/feeds/feeds.ts
+++ g00339629/src/providers/feeds/feeds.ts
@@ -14,23 +14,21 @@ export class FeedsProvider {
     private apiURL:string = "https://api.rss2json.com/v1/api.json"
     private apiKey:string = "72nhr3wkeukkx6pv9kz8p8cgu8doh9qbhrepco0x"
     // setings
-    private rss = 'rss_url='            // url to be converted to JSON (must be escaped)
+    private rss_url = 'rss_url='            // url to be converted to JSON (must be escaped)
     private order_by = 'order_by='      // Possible values : pubDate, author or title.
     private order_dir = 'order_dir='     // order direction 'desc' or 'asc' (default 'desc')
     private count = 'count='             // Count of feed items to return, default is 10 .
 
-    // https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.reuters.com%2Freuters%2FtechnologyNews
-
-    constructor(public http: HttpClient
-        // , private feed:Feed
+    constructor(
+        public http: HttpClient
     ){
         console.log('Hello FeedsProvider Provider');
     }
 
     // https://stackoverflow.com/a/332888/5322506
     getFeed = (rssURL:string):Observable<any> =>
-            this.http.get(`${this.apiURL}?${this.apiKey}&${this.rss}${encodeURIComponent(rssURL)}`
+            this.http.get(`${this.apiURL}?${this.apiKey}&${this.rss_url}${encodeURIComponent(rssURL)}`
             +`&${this.count}13`)
-                    // .map(it => it)
+
 
 }
